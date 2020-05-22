1fca952f8ac03812cc0212805afc031f0ca7badc
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-05-21 20:15:34 -0500

    Se agrega marcador al mapa con objetos para la latitud, longitud y el label

--- src/app/components/map-web/map-web.component.ts
+++ src/app/components/map-web/map-web.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapWebComponent implements OnInit {
 
+  title: string = 'gmaps';
+
+  position = {
+    lat: 51.678418,
+    lng: 7.809007
+  }
+
+  label = {
+    color: 'blue',
+    text: 'Marcador'
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
