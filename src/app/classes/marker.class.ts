041653502521db3e8eb617dc95ae6a5e969cf58b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-05-23 22:43:52 -0500

    Se hizo mapa con leaflet y se importo con npm install leaflet

--- src/app/classes/marker.class.ts
+++ src/app/classes/marker.class.ts
@@ -2,11 +2,11 @@ export class Marker {
 
   public lat: number;
   public lng: number;
-  public title: string;
+  // public title: string = 'Sin título';
+  // public description: string = 'Sin descripción';
 
-  constructor(lat: number, lng: number, title: string) {
+  constructor(lat: number, lng: number) {
     this.lat = lat;
     this.lng = lng;
-    this.title = title;
   }
 }
--- src/app/components/map-web/map-web.component.ts
+++ src/app/components/map-web/map-web.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Marker } from '../../classes/marker.class';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-web',
@@ -10,28 +13,100 @@ import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 })
 export class MapWebComponent implements OnInit {
 
-  @ViewChild(MapInfoWindow, {static: false}) infoWindow: MapInfoWindow;
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
 
-  title: string = 'gmaps';
-  zoom: number = 6;
+  // markers: Marker[] = [];
 
-  position = {
-    lat: 51.678418,
-    lng: 7.809007
+  // title: string = 'gmaps';
+  // zoom: number = 6;
+
+  // position = {
+  //   lat: 51.678418,
+  //   lng: 7.809007
+  // }
+
+  // label = {
+  //   color: 'blue',
+  //   text: 'Marcador'
+  // }
+
+  map;
+
+  smallIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
+
+  constructor() {
+    // const newMarker = new Marker(51.678418, 7.809007);
+    // console.log(newMarker);
+    // this.markers.push(newMarker);
   }
-
-  label = {
-    color: 'blue',
-    text: 'Marcador'
-  }
-
-  constructor() { }
 
   ngOnInit(): void {
+    this.createMap();
   }
 
-  openInfoWindow(marker: MapMarker) {
-    this.infoWindow.open(marker);
+  createMap() {
+    const parcThabor = {
+      lat: 48.114384,
+      lng: -1.669494
+
+      // lat: 5.073378,
+      // lng: -75.498967
+    };
+    const zoomLevel = 12;
+
+    this.map = L.map('map', {
+      center: [parcThabor.lat, parcThabor.lng],
+      zoom: zoomLevel
+    });
+
+    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 1,
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    mainLayer.addTo(this.map);
+
+    const descriptionWikipedia = `
+      Le parc du Thabor, situé à Rennes à proximité du centre-ville,
+      est un parc public aménagé sur plus de dix hectares dont la particularité est de mêler un jardin à la française,
+      un jardin à l’anglaise et un important jardin botanique.`;
+    const popupOptions = {
+      coords: parcThabor,
+      text: descriptionWikipedia,
+      open: true
+    };
+    this.addMarker(popupOptions);
   }
+
+  addMarker({ coords, text, open }) {
+    const marker = L.marker(
+      [coords.lat, coords.lng],
+      { icon: this.smallIcon }
+    );
+
+    if (open) {
+      marker.addTo(this.map).bindPopup(text).openPopup();
+    } else {
+      marker.addTo(this.map).bindPopup(text);
+    }
+  }
+
+  // openInfoWindow(marker: MapMarker) {
+  //   console.log(marker);
+  //   this.infoWindow.open(marker);
+  // }
+
+  // addMarker(event: Event) {
+  //   console.log(event);
+  // }
 
 }
